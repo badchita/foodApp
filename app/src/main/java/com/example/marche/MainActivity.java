ad0ebe518c73e99cc84dbdb3be142f5aa81d5fef
airamxx <60055020+airamxx@users.noreply.github.com>
2020-12-09 14:24:40 +0800

    initial push

---
+++ app/src/main/java/com/example/marche/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.marche;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        image = findViewById(R.id.imageView);
+
+        image.setAnimation(topAnim);
+
+    }
+}
